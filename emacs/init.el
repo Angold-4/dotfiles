5eb1e0b2ff9d7d499a5576024b357fea0faa670e
Angold-4 <wangold4w@gmail.com>
2022-04-11 10:50:32 +0800

    emacs

---
+++ emacs/init.el
@@ -0,0 +1,49 @@
+(setq inhibit-startup-message t)
+
+(setq mac-option-modifier 'meta) ; set alt-key to meta
+(setq mac-escape-modifier nil) ; set esc-key to nil
+
+;; Set up the visible bell
+(setq visible-bell t)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("org" . "https://orgmode.org/elpa/")
+			 ("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package command-log-mode)
+
+ (use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1)
+
+
+  
+
